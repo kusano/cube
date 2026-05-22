4bbe7963fe1937b0bbc5f01b7985c3a28eaedbe1
kusano <kusano@users.noreply.github.com>
2026-05-22 21:16:58 +0900

    Linear FMC Counter

---
+++ linear_fmc_counter/script.js
@@ -0,0 +1,136 @@
+// Sample:
+// https://alg.cubing.net/?alg=L%2F*0*%2F%20D%2F*2106*%2F%20R%27%2F*3166*%2F%20D%27%2F*3771*%2F%20B%2F*4847*%2F%20L%2F*10543*%2F%20U%2F*16462*%2F%20R%2F*16968*%2F%20D%27%2F*24682*%2F%20F%2F*25347*%2F%20F%2F*25410*%2F%20D%27%2F*29962*%2F%20L%2F*30906*%2F%20L%2F*30978*%2F%20R%2F*34218*%2F%20R%2F*34272*%2F%20U%2F*35917*%2F%20R%2F*37087*%2F%20R%2F*37142*%2F%20U%2F*37844*%2F%20U%2F*37928*%2F%20D%27%2F*38780*%2F%20L%2F*39333*%2F%20R%2F*55257*%2F%20R%2F*55309*%2F%20U%2F*56327*%2F%20R%2F*61779*%2F%20R%2F*61833*%2F%20R%27%2F*64713*%2F%20R%27%2F*64758*%2F%20U%2F*66553*%2F%20U%2F*66637*%2F%20F%2F*67791*%2F%20F%2F*67843*%2F%20F%2F*72231*%2F%20F%2F*72290*%2F%20U%2F*75049*%2F%20U%27%2F*83739*%2F%20B%2F*91228*%2F%20B%2F*91320*%2F%20U%27%2F*92918*%2F%20U%2F*94524*%2F%20U%2F*94778*%2F%20B%2F*95324*%2F%20B%2F*95404*%2F%20U%2F*96025*%2F%20R%2F*101488*%2F%20R%2F*101538*%2F%20L%2F*101896*%2F%20L%2F*102208*%2F%20U%2F*102556*%2F%20F%2F*103066*%2F%20F%2F*103169*%2F%20B%2F*103778*%2F%20B%2F*103896*%2F%20D%27%2F*104459*%2F%20U%2F*124615*%2F%20U%2F*124699*%2F%20R%2F*135908*%2F%20R%2F*135967*%2F%20F%2F*136856*%2F%20F%2F*136937*%2F%20U%2F*138213*%2F%20U%2F*138275*%2F%20R%2F*144827*%2F%20R%2F*144874*%2F%20B%2F*145312*%2F%20B%2F*145397*%2F%20U%2F*146185*%2F%20U%2F*146264*%2F%20L%2F*147789*%2F%20L%2F*147879*%2F%20F%2F*151182*%2F%20F%2F*151245*%2F%20U%2F*151831*%2F%20D%27%2F*152154*%2F%20L%2F*152578*%2F%20L%2F*152669*%2F%20U%27%2F*153292*%2F%20D%2F*153712*%2F&setup=R%27%20U%27%20F%20U2%20B%27%20D2%20B2%20L2%20R2%20F%27%20L2%20F%20U2%20R2%20U2%20L%20R%27%20U%27%20F%20D%20U%27%20R%20F2%20R%27%20U%27%20F
+// https://alg.cubing.net/?alg=L%2F*0*%2F_D%2F*2106*%2F_R-%2F*3166*%2F_D-%2F*3771*%2F_B%2F*4847*%2F_L%2F*10543*%2F_U%2F*16462*%2F_R%2F*16968*%2F_D-%2F*24682*%2F_F%2F*25347*%2F_F%2F*25410*%2F_D-%2F*29962*%2F_L%2F*30906*%2F_L%2F*30978*%2F_R%2F*34218*%2F_R%2F*34272*%2F_U%2F*35917*%2F_R%2F*37087*%2F_R%2F*37142*%2F_U%2F*37844*%2F_U%2F*37928*%2F_D-%2F*38780*%2F_L%2F*39333*%2F_R%2F*55257*%2F_R%2F*55309*%2F_U%2F*56327*%2F_R%2F*61779*%2F_R%2F*61833*%2F_R-%2F*64713*%2F_R-%2F*64758*%2F_U%2F*66553*%2F_U%2F*66637*%2F_F%2F*67791*%2F_F%2F*67843*%2F_F%2F*72231*%2F_F%2F*72290*%2F_U%2F*75049*%2F_U-%2F*83739*%2F_B%2F*91228*%2F_B%2F*91320*%2F_U-%2F*92918*%2F_U%2F*94524*%2F_U%2F*94778*%2F_B%2F*95324*%2F_B%2F*95404*%2F_U%2F*96025*%2F_R%2F*101488*%2F_R%2F*101538*%2F_L%2F*101896*%2F_L%2F*102208*%2F_U%2F*102556*%2F_F%2F*103066*%2F_F%2F*103169*%2F_B%2F*103778*%2F_B%2F*103896*%2F_D-%2F*104459*%2F_U%2F*124615*%2F_U%2F*124699*%2F_R%2F*135908*%2F_R%2F*135967*%2F_F%2F*136856*%2F_F%2F*136937*%2F_U%2F*138213*%2F_U%2F*138275*%2F_R%2F*144827*%2F_R%2F*144874*%2F_B%2F*145312*%2F_B%2F*145397*%2F_U%2F*146185*%2F_U%2F*146264*%2F_L%2F*147789*%2F_L%2F*147879*%2F_F%2F*151182*%2F_F%2F*151245*%2F_U%2F*151831*%2F_D-%2F*152154*%2F_L%2F*152578*%2F_L%2F*152669*%2F_U-%2F*153292*%2F_D%2F*153712*%2F&setup=R-_U-_F_U2_B-_D2_B2_L2_R2_F-_L2_F_U2_R2_U2_L_R-_U-_F_D_U-_R_F2_R-_U-_F
+// L D R' D' B L U R D' F F D' L L R R U R R U U D' L R R U R R R' R' U U F F F F U U' B B U' U U B B U R R L L U F F B B D' U U R R F F U U R R B B U U L L F F U D' L L U' D
+
+document.addEventListener("DOMContentLoaded", () => {
+    function elem(id) {
+        return document.getElementById(id);
+    }
+
+    elem("input").focus();
+
+    function log(message) {
+        const div = document.createElement("div");
+        elem("log").appendChild(div);
+        div.textContent = message;
+    }
+
+    elem("input").addEventListener("input", () => {
+        const input = elem("input").value.trim().replaceAll("‘", "'").replaceAll("’", "'");;
+
+        while (elem("log").firstChild) {
+            elem("log").removeChild(elem("log").firstChild);
+        }
+        elem("time").textContent = "";
+        elem("result").textContent = "";
+        elem("solution").textContent = "";
+
+        let solution = "";
+        if (input.startsWith("https://alg.cubing.net/")) {
+            const url = new URL(input);
+            const setup = url.searchParams.get("setup");
+            if (setup) {
+                log(`Setup: ${setup}`);
+            }
+            solution = url.searchParams.get("alg");
+            if (!solution) {
+                log("alg not found");
+                return;
+            }
+        } else {
+            solution = input;
+        }
+        solution = solution.replaceAll("_", " ").replaceAll("-", "'");
+        log(`Solution: ${solution}`);
+
+        const parsed = [];
+        for (const move of solution.split(/ |\/\*.*?\*\//)) {
+            if (move.match(/^[UDRLFB](|'|2)$/)) {
+                parsed.push(move);
+            }
+        }
+        log(`Parsed solution: [${parsed.join(", ")}]`);
+
+        let time = -1;
+        for (const match of solution.matchAll(/\/\*(\d+)\*\//g)) {
+            time = Math.max(time, +match[1]);
+        }
+        log(`Time: ${time}`);
+
+        const normalized = [];
+        for (const move of parsed) {
+            if (normalized.length==0) {
+                normalized.push(move);
+            } else {
+                const prev = normalized.pop();
+                if (prev[0]!=move[0]) {
+                    normalized.push(prev);
+                    normalized.push(move);
+                } else {
+                    function power(x) {
+                        if (x.length==1) {
+                            return 1;
+                        } else if (x[1]=="0") {
+                            return 0;
+                        } else if (x[1]=="2") {
+                            return 2;
+                        } else if (x[1]=="'") {
+                            return 3;
+                        }
+                    }
+
+                    const n1 = power(prev);
+                    const n2 = power(move);
+                    const n = (n1+n2)%4;
+                    normalized.push(move[0]+["0", "", "2", "'"][n]);
+                }
+            }
+        }
+        log(`Normalized solution: [${normalized.join(", ")}]`);
+
+        let result = 0;
+        for (const move of normalized) {
+            if (move.length==1 || move[1]!="0") {
+                result++;
+            }
+        }
+        log(`Result: ${result}`);
+
+        if (time>=0) {
+            const min = time/60000|0;
+            const sec = (time/1000|0)%60;
+            const ms = time%1000;
+
+            function pad(x, d) {
+                let r = ""+x;
+                while (r.length<d) {
+                    r = "0"+r;
+                }
+                return r;
+            }
+            elem("time").textContent = `${min}:${pad(sec, 2)}.${pad(ms, 3)}`;
+        } else {
+            elem("time").textContent = "-";
+        }
+        elem("result").textContent = ""+result;
+        elem("solution").textContent = normalized.join(" ");
+    });
+
+    elem("copy").addEventListener("click", async () => {
+        elem("copy").setAttribute("disabled", "");
+        elem("copy").classList.add("is-loading");
+
+        setTimeout(() => {
+            elem("copy").removeAttribute("disabled");
+            elem("copy").classList.remove("is-loading");
+        }, 300);
+
+        let text = "";
+        text += `Time: ${elem("time").textContent}\n`;
+        text += `Result: ${elem("result").textContent}\n`;
+        text += `Solution: ${elem("solution").textContent}\n`;
+
+        await navigator.clipboard.writeText(text);
+    });
+});
